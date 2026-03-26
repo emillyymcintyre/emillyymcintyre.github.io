b91339e601406fd448bb095fffc6c9f93a49969d
Emily McIntyre <emillyymcintyre@gmail.com>
2026-03-25 23:16:24 -0600

    ica 11

---
+++ ICA11/script.js
@@ -0,0 +1,55 @@
+// Complete variable definitions and random function
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// Solution: Raw text strings
+
+const characters = ["Emily Mac", "Big Daddy", "Evil Poo Man"];
+const places = ["evil house", "cat land", "kitty kat palace"];
+const events = [
+  "pooped and farted",
+  "exploded and died",
+  "meowed like a cat",
+];
+
+// Solution: Partial return random string function
+
+function returnRandomStoryString() {
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+
+  let storyText = `It was 0 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+
+  return storyText;
+}
+
+// Solution: Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  let newStory = returnRandomStoryString();
+
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("0 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
